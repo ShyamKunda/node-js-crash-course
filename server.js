9566df11e99df6dafc6fbe7bfebf5474c5444a8a
Shyam Kunda <shyam.kunda@prismforce.ai>
2025-01-19 07:40:42 +0530

    Added Middleware and refactor

--- server.js
+++ server.js
@@ -4,27 +4,52 @@ import getUsers from './usersController.js';
 
 const PORT = 8000;
 
-const server = http.createServer(async (req, res) => {
+const logger = (req, res, next) => {
+    console.log(`${req.method} ${req.url}`);
+    next();
+  };
 
-    if (req.url === "/") {
-        res.setHeader('Content-Type', 'text/html');
-        res.write("Hello world")
-        res.end();
-    } else if (req.url === '/api/users' && req.method === 'GET'){
-        res.setHeader('Content-Type', 'application/json');
-        res.write(JSON.stringify(getUsers()));
-        res.end()
-    } else if (req.url.match(/\/api\/users\/([0-9]+)/) &&req.method === 'GET') {
-        res.setHeader('Content-Type', 'application/json');
-        const targetId = req.url.split("/")[3];
-        console.log(`Target Id : ${targetId}`)
-        const users = getUsers();
-        console.log(users);
-        const targetUser  = users.filter(user => user.id == targetId);
-        console.log(targetUser);
-        res.write(JSON.stringify(targetUser));
-        res.end()
-    }
+// JSON middleware
+const jsonMiddleware = (req, res, next) => {
+    res.setHeader('Content-Type', 'application/json');
+    next();
+};
+
+// Route handler for GET /api/users
+const getUsersHandler = (req, res) => {
+    const users = getUsers();
+    console.log(JSON.stringify(users))
+    res.write(JSON.stringify(users));
+    res.end();
+};
+
+const getUsersByIdHandler = (req, res) => {
+    res.setHeader('Content-Type', 'application/json');
+    const targetId = req.url.split("/")[3];
+    console.log(`Target Id : ${targetId}`)
+    const users = getUsers();
+    console.log(users);
+    const targetUser  = users.filter(user => user.id == targetId);
+    console.log(targetUser);
+    res.write(JSON.stringify(targetUser));
+    res.end()
+}
+
+const server = http.createServer(async (req, res) => {
+    logger(req, res, () => {
+        jsonMiddleware(req, res, () => {
+            if (req.url === "/") {
+                res.write("Hello world")
+                res.end();
+            } else if (req.url === '/api/users' && req.method === 'GET'){
+                getUsersHandler(req, res)
+            } else if (req.url.match(/\/api\/users\/([0-9]+)/) &&req.method === 'GET') {
+                getUsersByIdHandler(req, res)
+            }
+        });
+    });
+    
+    
     
 })
 
